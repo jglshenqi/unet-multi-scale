8a6f1e6afe7333010348441dcd3575d0f2de6b66
jglshenqi <36980202+jglshenqi@users.noreply.github.com>
2018-09-06 11:04:23 +0800

    Delete read_config.py

--- read_config.py
+++
@@ -1,6 +0,0 @@
-import configparser
-from keras.callbacks import ModelCheckpoint, Callback
-import network
-from extract_patches import get_data_training
-from help_functions import *
-import skimage.measure
